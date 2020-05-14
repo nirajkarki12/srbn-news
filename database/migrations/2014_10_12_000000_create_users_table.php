58b6871d018ccf139b69e64511078fb0127107a7
Niraj Karki <nirajkarki12@gmail.com>
2020-05-14 20:16:58 +0545

    changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,7 +28,7 @@ class CreateUsersTable extends Migration
 
             $table->string('social_id')->nullable();
             $table->string('provider')->nullable();
-            $table->bigInteger('phone')->nullable()->unique();
+            $table->integer('phone')->nullable()->unique();
 
             $table->rememberToken();
             $table->timestamps();
